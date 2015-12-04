4d0f0330560e10f8d0c130de0c7ea589c95ce651
abekei0317 <abekei0317@gmail.com>
2015-12-04 06:30:15 +0000

    add ranking_page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
+  get    'ranking/want', to: 'rankings#want'
+  get    'ranking/have', to: 'rankings#have'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
